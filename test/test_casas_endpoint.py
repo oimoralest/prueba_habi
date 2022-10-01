7aad76f4c010febca7fd136c70bfadd8148bb358
oimoralest <oimoralest@gmail.com>
2022-10-01 18:32:40 -0500

    Agregando mas unit tests para endpoint /casas/enventa

--- test/test_casas_endpoint.py
+++ test/test_casas_endpoint.py
@@ -129,6 +129,24 @@ class CasasEndpointTestCase(unittest.TestCase):
                 f"Expected year {city} but got {casa['city']}",
             )
 
+    def test_casas_preventa_status_code_204(self):
+        """
+        Test:
+            * /casas/preventa endpoint with not valid filter
+
+        Expected results:
+            * response status code equals to 200
+        """
+        response = requests.get(
+            url=f"http://{HOST}:{PORT}/casas/preventa", params={"foo": "bar"}
+        )
+
+        self.assertEqual(
+            response.status_code,
+            204,
+            f"Expected status code 200 but got {response.status_code}",
+        )
+
     def test_casas_enventa_status_code_200(self):
         """
         Test:
@@ -142,5 +160,115 @@ class CasasEndpointTestCase(unittest.TestCase):
         self.assertEqual(
             response.status_code,
             200,
+            f"Expected status code 200 but got {response.status_code}",
+        )
+
+    def test_casas_enventa_content_response(self):
+        """
+        Test:
+            * /casas/enventa endpoint
+
+        Expected results:
+            * content response only contents casas with status equal to
+            en_venta
+        """
+        response = requests.get(url=f"http://{HOST}:{PORT}/casas/enventa")
+
+        data = response.json()
+        for casa in data:
+            self.assertEqual(
+                casa["status"],
+                "en_venta",
+                f"Expected status 'pre_venta' but got {casa['status']}",
+            )
+
+    def test_casas_enventa_content_response_city_and_addres_not_null(self):
+        """
+        Test:
+            * /casas/enventa endpoint
+
+        Expected results:
+            * content response only contents casas with non empty address and
+            city
+        """
+        response = requests.get(url=f"http://{HOST}:{PORT}/casas/enventa")
+
+        data = response.json()
+        for casa in data:
+            self.assertNotIn(
+                casa["address"],
+                ("", None),
+                "Expected address not empty but it's empty",
+            )
+            self.assertNotIn(
+                casa["city"],
+                ("", None),
+                "Expected city not empty but it's empty",
+            )
+
+    def test_casas_enventa_year_filter(self):
+        """
+        Test:
+            * /casas/enventa endpoint
+
+        Expected results:
+            * content response only contents with filter by year condition
+        """
+        year = 2011
+        response = requests.get(
+            url=f"http://{HOST}:{PORT}/casas/enventa", params={"year": year}
+        )
+
+        data = response.json()
+        for casa in data:
+            self.assertEqual(
+                casa["year"],
+                year,
+                f"Expected year {year} but got {casa['year']}",
+            )
+
+    def test_casas_enventa_year_and_city_filter(self):
+        """
+        Test:
+            * /casas/enventa endpoint
+
+        Expected results:
+            * content response only contents with filter by year condition
+        """
+        year = 2011
+        city = "medellin"
+        response = requests.get(
+            url=f"http://{HOST}:{PORT}/casas/enventa",
+            params={"year": year, "city": city},
+        )
+
+        data = response.json()
+        for casa in data:
+            self.assertEqual(
+                casa["year"],
+                year,
+                f"Expected year {year} but got {casa['year']}",
+            )
+            self.assertEqual(
+                casa["city"],
+                city,
+                f"Expected year {city} but got {casa['city']}",
+            )
+
+    def test_casas_enventa_status_code_204(self):
+        """
+        Test:
+            * /casas/enventa endpoint with not valid filter
+
+        Expected results:
+            * response status code equals to 200
+        """
+        response = requests.get(
+            url=f"http://{HOST}:{PORT}/casas/enventa", params={"foo": "bar"}
+        )
+
+        self.assertEqual(
+            response.status_code,
+            204,
             f"Expected status code 200 but got {response.status_code}",
         )
